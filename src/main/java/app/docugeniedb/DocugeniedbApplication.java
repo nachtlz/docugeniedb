e3443fa879790455db6e092dbd27d53ffe7c8fc1
nachtlz <juanignaciolopezb@gmail.com>
2024-04-10 12:46:27 +0200

    Configured for service registry

--- src/main/java/app/docugeniedb/DocugeniedbApplication.java
+++ src/main/java/app/docugeniedb/DocugeniedbApplication.java
@@ -2,8 +2,10 @@ package app.docugeniedb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DocugeniedbApplication {
 
     public static void main(String[] args) {
